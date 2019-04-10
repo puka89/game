13b4fd8dcd31b0bbd22cced4fc4e61d94eac8ee4
puka89 <kristiqn.stoimenov.tues@gmail.com>
2019-04-09 22:52:22 -0700

    initial commit

---
+++ Assets/events.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class events : MonoBehaviour
+{
+    private void Start()
+    {
+        foreach (string str in Input.GetJoystickNames())
+        {
+            Debug.Log("Connected: " + str);
+        }
+    }
+
+
+    void Update()
+    {
+        return;
+        System.Array values = System.Enum.GetValues(typeof(KeyCode));
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKeyDown(code)) { Debug.Log(System.Enum.GetName(typeof(KeyCode), code)); }
+        }
+    }
+}
